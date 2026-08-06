3b969bca1e5f94b67fc4abbb34a52f80a3c26530
muhammad.a2 <muhammad.a@snoonu.com>
2026-08-07 02:37:36 +0300

    Switch to openai embed model

--- data_loader.py
+++ data_loader.py
@@ -1,12 +1,16 @@
-from sentence_transformers import SentenceTransformer
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
 from llama_index.readers.file import PDFReader
 from llama_index.core.node_parser import SentenceSplitter
 
-EMBED_MODEL = "all-mpnet-base-v2"
+load_dotenv()
+
+EMBED_MODEL = "text-embedding-3-small"
 EMBED_DIM = 768
 
 splitter = SentenceSplitter(chunk_size=1000, chunk_overlap=200)
-_model = SentenceTransformer(EMBED_MODEL)
+_model = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 def load_and_chunk_pdf(path: str):
     docs = PDFReader().load_data(file=path)
@@ -17,4 +21,7 @@ def load_and_chunk_pdf(path: str):
     return chunks
 
 def embed_texts(texts: list[str]) -> list[list[float]]:
-    return _model.encode(texts, convert_to_numpy=True).tolist()
+    response = _model.embeddings.create(
+          input=texts, model=EMBED_MODEL, dimensions=EMBED_DIM
+    )
+    return [data.embedding for data in sorted(response.data, key=lambda x: x.index) ]
